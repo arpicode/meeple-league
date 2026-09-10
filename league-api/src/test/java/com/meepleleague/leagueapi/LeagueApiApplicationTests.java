1151a772503ceaf1a4201993e6bc8b56036cbf42
arpicode <arpicode@gmail.com>
2026-09-10 16:22:10 +0200

    chore: init spring boot project

---
+++ league-api/src/test/java/com/meepleleague/leagueapi/LeagueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meepleleague.leagueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
