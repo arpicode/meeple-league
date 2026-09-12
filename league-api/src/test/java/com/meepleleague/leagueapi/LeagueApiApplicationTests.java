22b500d27c293719492af47d3fd5303bce2cddf1
arpicode <arpicode@gmail.com>
2026-09-12 15:34:39 +0200

    test: postgres testcontainers setup

--- league-api/src/test/java/com/meepleleague/leagueapi/LeagueApiApplicationTests.java
+++ league-api/src/test/java/com/meepleleague/leagueapi/LeagueApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.meepleleague.leagueapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class LeagueApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
